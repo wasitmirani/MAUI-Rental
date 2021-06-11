520a68885973ac6667be48c0087cac0a5a91a684
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-06-12 00:58:59 +0500

    csrf token added

--- resources/views/layouts/member/master.blade.php
+++ resources/views/layouts/member/master.blade.php
@@ -5,6 +5,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>{{config('app.name')}}</title>
     <!-- Favicon icon -->
